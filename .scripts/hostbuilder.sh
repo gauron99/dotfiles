cfaf447c15436345389b71404edf09b0f3f9ac05
David Fridrich <fridrich.david19@gmail.com>
2024-09-12 09:02:38 +0200

    Update dotfiles

---
+++ .scripts/hostbuilder.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# this just sets all the variables needed to run the host builder by default
+
+# always source this file ('source funchere' or '. funchere')
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "This script should be sourced, not executed."
+    exit 1
+fi
+export FUNC_ENABLE_HOST_BUILDER=1 FUNC_BUILDER=host FUNC_CONTAINER=false
